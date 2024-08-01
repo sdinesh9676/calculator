f58ac8c750929bc59016bf8cdcf3312cb42532b1
sdinesh9676 <167186574+sdinesh9676@users.noreply.github.com>
2024-08-01 12:11:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,131 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const calculator = {
+        displayValue: '0',
+        firstOperand: null,
+        waitingForSecondOperand: false,
+        operator: null,
+    };
+
+    function updateDisplay() {
+        const display = document.querySelector('.calculator-screen');
+        display.value = calculator.displayValue;
+    }
+
+    updateDisplay();
+
+    const keys = document.querySelector('.calculator-keys');
+    keys.addEventListener('click', (event) => {
+        const { target } = event;
+        const { value } = target;
+
+        if (!target.matches('button')) {
+            return;
+        }
+
+        switch (value) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '=':
+                handleOperator(value);
+                break;
+            case '.':
+                inputDecimal(value);
+                break;
+            case 'all-clear':
+                resetCalculator();
+                break;
+            default:
+                if (Number.isInteger(parseFloat(value))) {
+                    inputDigit(value);
+                }
+        }
+
+        updateDisplay();
+    });
+
+    function inputDigit(digit) {
+        const { displayValue, waitingForSecondOperand } = calculator;
+
+        if (waitingForSecondOperand === true) {
+            calculator.displayValue = digit;
+            calculator.waitingForSecondOperand = false;
+        } else {
+            calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+        }
+    }
+
+    function inputDecimal(dot) {
+        if (calculator.waitingForSecondOperand === true) {
+            calculator.displayValue = '0.';
+            calculator.waitingForSecondOperand = false;
+            return;
+        }
+
+        if (!calculator.displayValue.includes(dot)) {
+            calculator.displayValue += dot;
+        }
+    }
+
+    function handleOperator(nextOperator) {
+        const { firstOperand, displayValue, operator } = calculator;
+        const inputValue = parseFloat(displayValue);
+
+        if (operator && calculator.waitingForSecondOperand) {
+            calculator.operator = nextOperator;
+            return;
+        }
+
+        if (firstOperand == null) {
+            calculator.firstOperand = inputValue;
+        } else if (operator) {
+            const currentValue = firstOperand || 0;
+            const result = performCalculation[operator](currentValue, inputValue);
+
+            calculator.displayValue = String(result);
+            calculator.firstOperand = result;
+        }
+
+        calculator.waitingForSecondOperand = true;
+        calculator.operator = nextOperator;
+    }
+
+    const performCalculation = {
+        '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+        '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+        '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+        '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+        '=': (firstOperand, secondOperand) => secondOperand,
+    };
+
+    function resetCalculator() {
+        calculator.displayValue = '0';
+        calculator.firstOperand = null;
+        calculator.waitingForSecondOperand = false;
+        calculator.operator = null;
+    }
+
+    const themeToggle = document.getElementById('themeToggle');
+    const calculatorElement = document.getElementById('calculator');
+    const numberButtons = document.querySelectorAll('.number');
+    const display = document.querySelector('.calculator-screen');
+    themeToggle.addEventListener('click', () => {
+        if (calculatorElement.style.backgroundColor === 'black') {
+            calculatorElement.style.backgroundColor = '#ffffff';
+            themeToggle.classList.remove('light');
+            numberButtons.forEach(button => {
+                button.classList.remove('light');
+            });
+            display.classList.remove('light');
+        } else {
+            calculatorElement.style.backgroundColor = 'black';
+            themeToggle.classList.add('light');
+            numberButtons.forEach(button => {
+                button.classList.add('light');
+            });
+            display.classList.add('light');
+        }
+    });
+});
